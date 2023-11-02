359b28090489a3b7c3ecf669203b0485047fd4b8
nhanphuong <trannhan.s3@gmail.com>
2023-11-02 22:53:42 +0700

    02.11.2023 - hoan thanh login back end

---
+++ laravel/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        if (!Auth::attempt($request->only(['username', 'password']))) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Tài khoản & Mật khẩu không đúng.',
+            ], 401);
+        }
+        $user = User::where('username', $request->username)->where('isActive', true)->with(['roles' => function ($s) {
+            $s->select('id', 'name');
+        }, 'department' => function ($s) {
+            $s->select('id', 'name', 'alias');
+        }])->first();
+        if (!$user) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Tài khoản đang tạm khoá.',
+            ], 401);
+        }
+        return response()->json([
+            'token' => $user->createToken("authToken")->plainTextToken,
+            'user' => $user
+        ], 200);
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CompanyController;
 use App\Http\Controllers\Api\DepartmentController;
 use App\Http\Controllers\Api\RoleController;
@@ -21,6 +22,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
 });
 
 Route::prefix('company')->group(function () {
