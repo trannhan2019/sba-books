3157be7cb461757fa49fca47f61c73b072dd1c83
trannhan2019 <53633928+trannhan2019@users.noreply.github.com>
2023-12-06 17:04:13 +0700

    06.12.2023 - hoan thanh giao dien chi tiet sach

--- react_mui/src/pages/private/book/detail.jsx
+++ react_mui/src/pages/private/book/detail.jsx
@@ -1,5 +1,4 @@
-import { useParams } from "react-router-dom";
-
+import { Link, useParams } from "react-router-dom";
 import {
   Box,
   Button,
@@ -13,6 +12,7 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useQuery } from "@tanstack/react-query";
 import { apiGetBook } from "@/apis/book";
 import BookImageDefault from "@/assets/default-image-book.jpg";
@@ -43,51 +43,91 @@ const BookDetail = () => {
         }}
       >
         <Container maxWidth="lg">
-          <Typography variant="h3">Thông tin chi tiết</Typography>
+          <Stack direction="row" mb={4} justifyContent="space-between">
+            <Typography variant="h3">Thông tin chi tiết</Typography>
+            <Link to="/">
+              <Button variant="outlined" startIcon={<ArrowBackIcon />}>
+                Trở lại
+              </Button>
+            </Link>
+          </Stack>
           <Card>
-            <Grid container mt={3}>
+            <Grid container>
               <Grid item xs={12} md={5}>
-                <CardMedia
+                {/* <Box
                   sx={{
                     backgroundImage: `url(${bookPhoto})`,
                     backgroundPosition: "center",
-                    backgroundSize: "cover",
+                    backgroundSize: "85% 85%",
                     borderRadius: 1,
                     height: 500,
                   }}
+                /> */}
+                <CardMedia
+                  image={bookPhoto}
+                  sx={{ height: 500, backgroundSize: "85% 85%" }}
                 />
               </Grid>
 
               <Grid item xs={12} md={7}>
                 <CardContent>
-                  <Stack spacing={3}>
-                    <Typography variant="h3">{book?.title}</Typography>
-                    <div>
-                      <Chip label={book?.cate_book?.name} />
-                    </div>
-                    <Typography color="text.secondary" variant="subtitle1">
+                  <Stack spacing={1}>
+                    <Typography mb={2} variant="h3">
+                      {book?.title}
+                    </Typography>
+
+                    <Stack alignItems="center" direction="row" spacing={2}>
+                      <Typography variant="subtitle2">
+                        Thuộc thể loại:
+                      </Typography>
+                      <Chip
+                        color="info"
+                        size="small"
+                        label={book?.cate_book?.name}
+                      />
+                    </Stack>
+                    <Typography variant="subtitle2">
+                      Tác giả: {book?.author}
+                    </Typography>
+
+                    <Stack alignItems="center" direction="row" spacing={2}>
+                      <Typography variant="subtitle2">Số lượng:</Typography>
+                      {book?.quantity > 0 ? (
+                        <Chip size="small" label={book?.quantity} />
+                      ) : (
+                        <Chip size="small" color="error" label="Đã mượn hết" />
+                      )}
+                    </Stack>
+
+                    <Typography variant="subtitle2">
+                      Mã sách: {book?.code}
+                    </Typography>
+
+                    <Typography variant="subtitle2">
+                      Thông tin lưu trữ: {book?.storage_location}
+                    </Typography>
+
+                    <Typography variant="subtitle2">
                       {book?.description}
                     </Typography>
-                    <Stack
-                      alignItems="center"
-                      direction="row"
-                      spacing={2}
-                      sx={{ mt: 3 }}
-                    >
-                      {/* <Avatar src={post.author.avatar} />
-                <div>
-                  <Typography variant="subtitle2">
-                    By {post.author.name} • {publishedAt}
-                  </Typography>
-                  <Typography color="text.secondary" variant="body2">
-                    {post.readTime} read
-                  </Typography>
-                </div> */}
-                    </Stack>
                   </Stack>
                 </CardContent>
-                <CardActions>
-                  <Button size="small">Muon</Button>
+                <CardActions sx={{ padding: 3 }}>
+                  {book?.quantity > 0 ? (
+                    <Button variant="contained" size="large" fullWidth>
+                      Mượn sách
+                    </Button>
+                  ) : (
+                    <Button
+                      disabled
+                      color="error"
+                      variant="contained"
+                      size="large"
+                      fullWidth
+                    >
+                      Sách đã mượn hết
+                    </Button>
+                  )}
                   {/* disable khi so luong = 0 */}
                 </CardActions>
               </Grid>
